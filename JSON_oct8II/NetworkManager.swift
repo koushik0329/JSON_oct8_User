7d2f510b17fd9b155ebb82c66afb4d4686982c12
koushik0329 <kkambham@islander.tamucc.edu>
2025-10-10 03:57:05 -0500

    error handling using state

--- JSON_oct8II/NetworkManager.swift
+++ JSON_oct8II/NetworkManager.swift
@@ -11,13 +11,27 @@ protocol NetworkManagerProtocol {
     func fetchUsers() async throws -> [User]
 }
 
+enum NetworkError : Error {
+    case invalidURL
+    case requestFailed
+    
+    var errorDesc: String? {
+        switch self {
+        case .invalidURL:
+            return "invalid URL"
+        case .requestFailed:
+            return "netwo swrk request failed"
+        }
+    }
+}
+
 final class NetworkManager : NetworkManagerProtocol {
     static let shared = NetworkManager()
     private init() {}
     
     func fetchUsers() async throws -> [User] {
         guard let url = URL(string: Server.endPoint.rawValue) else {
-            throw URLError(.badURL)
+            throw NetworkError.invalidURL
         }
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
@@ -26,7 +40,7 @@ final class NetworkManager : NetworkManagerProtocol {
             return users
         }
         catch {
-            throw error
+            throw NetworkError.requestFailed
         }
     }
 }
